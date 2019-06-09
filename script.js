5d10eea1f40402d7f4d0f41ea93acc813dc311dc
gerritt <gmocore@gmail.com>
2019-06-09 14:42:25 -0600

    fixed errant info in function

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
+const button = document.getElementById('button')
+const display = document.getElementById('hex-text')
+button.addEventListener('click', function() {
+    let hexadecimal = randomHex()
+    document.body.style.background = hexadecimal
+    display.innerHTML = hexadecimal
+})
 
-
-
-
-
+// let combined 
 function randomHex() {
     var hex = [];
-    const combo = ['a', 'b', 'c', 'd', 'e', 'f', 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  
-    for (let i = 0; i < 7; i++) {
-      
-      let randomThing = Math.floor(Math.random() * combo.length)
-      
-      hex.push(combo[randomThing])
-      
+    const combo = ['a', 'b', 'c', 'd', 'e', 'f', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    
+    for (let i = 0; i < 6; i++) {
+        
+        let randomThing = Math.floor(Math.random() * combo.length)
+        
+        hex.push(combo[randomThing])
     }
-      return hex.join('')
+        return `#${hex.join('')}`
+
   }
   
-  randomHex()
+  
